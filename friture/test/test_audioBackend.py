73adee57ee91b32bff124bfc5170b4f98c8596ac
foshay <foshayi@oregonstate.edu>
2019-03-07 12:48:13 -0800

    More tests written for abe

--- friture/test/test_audioBackend.py
+++ friture/test/test_audioBackend.py
@@ -1,1 +1,26 @@
 #Test file for audioBackend
+import pytest
+import friture.audiobackend as ab
+import sounddevice
+
+def test_creation():
+    abe = ab.AudioBackend()
+    assert abe != None
+
+def test_outputStreamOpen():
+    abe = ab.AudioBackend()
+    stream = abe.open_stream(abe.device)
+    assert stream != None
+
+def test_init():
+    abe = ab.AudioBackend()
+    abe.device = None
+    assert abe.device == None
+    abe.__init__()
+    assert abe.device != None
+
+def test_getInputDevices(monkeypatch):
+    abe = ab.AudioBackend()
+    monkeypatch.setattr("friture.audiobackend.__AudioBackend.get_input_devices", lambda x: [])
+    input_devices = abe.get_input_devices()
+    assert input_devices == []
