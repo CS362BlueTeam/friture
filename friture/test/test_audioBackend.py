018d9f7d4bf03341ac4dd7a54eed941acce3ad3d
foshay <foshayi@oregonstate.edu>
2019-03-07 18:37:45 -0800

    tests more of em

--- friture/test/test_audioBackend.py
+++ friture/test/test_audioBackend.py
@@ -8,8 +8,6 @@ def test_noAudioDevices(monkeypatch):
     monkeypatch.setattr("sounddevice.query_devices", lambda: [None])
     abe = ab.AudioBackend()
 
-
-
 def test_outputStreamOpen():
     abe = ab.AudioBackend()
     stream = abe.open_stream(abe.device)
@@ -55,3 +53,40 @@ def test_setSingleInput():
     abe = ab.AudioBackend()
     abe.set_single_input()
     assert abe.duo_input == False
+
+def test_handleNewData():
+    abe = ab.AudioBackend()
+    abe.handle_new_data("",[None],[None],True)
+    abe.handle_new_data("",[None],[None],False)
+
+def test_getters():
+    abe = ab.AudioBackend()
+    count = abe.get_device_outputchannels_count(abe.device)
+    assert count != None
+    count = abe.get_current_device_nchannels()
+    assert count != None
+    count = abe.get_current_second_channel()
+    assert count != None
+    count = abe.get_current_first_channel()
+    assert count != None
+    device = abe.get_readable_current_device()
+    assert device != None
+
+def test_currentChannels():
+    abe = ab.AudioBackend()
+    channels = abe.get_readable_current_channels()
+    assert channels != None
+    abe.device['max_input_channels'] = 2
+    channels = abe.get_readable_current_channels()
+    assert channels == ['L', 'R']
+
+def test_outputSupported():
+    abe = ab.AudioBackend()
+    assert abe.is_output_format_supported(abe.device, None) == False
+
+def test_selectInputDevice():
+    abe = ab.AudioBackend()
+    data = abe.select_input_device(0)
+    assert data != None
+    with pytest.raises(Exception):
+        abe.select_input_device(99999)
